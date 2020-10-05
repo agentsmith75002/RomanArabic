fee72452a08cca3e618c0d6cafdeb2b3e73b8346
Nicolas Lepinay <nicolas.lepinay@gmail.com>
2020-10-05 13:55:14 +0200

    Fix input console

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Logic;
 using System;
+using System.Linq;
 
 namespace Host
 {
@@ -7,10 +8,21 @@ namespace Host
     {
         public static void Main(string[] args)
         {
+            if( args == null || args.Length != 1)
+            {
+                Console.WriteLine("You must provide one roman number");
+                Environment.Exit(1);
+            }
+
             Engine engine = new Engine();
+            if(!args[0].ToArray().All(c=>engine.Evaluate(c) != -1))
+            {
+                Console.WriteLine("You must provide a valid roman number");
+                Environment.Exit(2);
+            }
 
             // Pas de vérification de nombre valide
-            string roman = "MCMIV";
+            string roman = args[0];
             Console.WriteLine($"'{roman}' = {engine.ComputeRomanToDecimal(roman)}");
             Console.ReadLine();
         }
